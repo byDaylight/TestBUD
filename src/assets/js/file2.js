36f5a331e0e037cd87e8ec6acf2cc1fd487d64d9
ReallyLuckyOne <38769994+ReallyLuckyOne@users.noreply.github.com>
2021-03-28 22:05:32 +0300

    responsive reaction

--- src/assets/js/file2.js
+++ src/assets/js/file2.js
@@ -5,7 +5,25 @@ $(document).ready(function () {
         adaptiveHeight: true,
         slidestoshow: 1,
         slidesToScroll: 1,
-        infinite: true
+        infinite: true,
+        mobileFirst: true,
+
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 878,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
+        ]
 
     });
 
@@ -13,26 +31,34 @@ $(document).ready(function () {
         arrows: false,
         dots: true,
         adaptiveHeight: true,
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
         infinite: true,
-
+        mobileFirst: true,
         responsive: [
             {
                 breakpoint: 787,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 2,
                     slidesToScroll: 1
                 }
             }
         ]
 
     });
+});
+
+/* unslick button */
+
+$('#unslick').on('click', function (e) {
+    e.preventDefault();
+    $('.reaction__slider').slick('unslick');
+    
 });
